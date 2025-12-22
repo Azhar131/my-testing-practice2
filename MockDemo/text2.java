051f8c1411d9a7614ec35778c30fd431a0ea347c
Mulla Azaruddin <mulla.azaruddin@nagarro.com>
2025-12-22 09:46:21 +0530

    AAA

---
+++ MockDemo/text2.java
@@ -0,0 +1,8 @@
+public class text2 {
+    public static void main(String[] args) {
+        System.out.print("Hello");        // no newline
+        System.out.print(" World");
+        System.out.println();             // newline
+        System.out.println("Hello World"); // prints with newline
+    }
+}
